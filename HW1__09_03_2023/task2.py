e093e4de99160ec1d124fb5675d4a2f84aedd7a3
Vladimir <vovar3573@mail.ru>
2023-03-11 15:08:26 +0300

    'task2_completed_kor'

--- HW1__09_03_2023/task2.py
+++ HW1__09_03_2023/task2.py
@@ -4,5 +4,5 @@ number = int(input('введите трехзначное число: '))
 if 99 < number < 1000:
     print(f'Сумма цифр числа {number} равна {number//100 + (number//10)%10 + number%10}')
 else:
-    print('ТРЕХЗНАЧНОЕ чиcло!')
+    print('Необходимо ввести ТРЕХЗНАЧНОЕ чиcло!')
 
