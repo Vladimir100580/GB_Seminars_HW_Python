2ae7628e9de8c1d1d54c43f41dd0cda03c134ee3
Vladimir <vovar3573@mail.ru>
2023-03-17 22:52:47 +0300

    'task20_completed'

--- Seminars/1.py
+++ Seminars/1.py
@@ -1,11 +1,14 @@
 # По данному целому неотрицательному n вычислите значение n!.
 # N! = 1 * 2 * 3 * … * N (произведение всех чисел от 1 до N) 0! = 1 Решить задачу используя цикл while
 
-number = int(input("Введите число"))
-s = 1
-while number > 1:
-    s *= number
-    number -= 1
-print("N!=", s)
+e = 'YРВАФ'
+print("A" in e)
+
+# number = int(input("Введите число"))
+# s = 1
+# while number > 1:
+#     s *= number
+#     number -= 1
+# print("N!=", s)
 
 
