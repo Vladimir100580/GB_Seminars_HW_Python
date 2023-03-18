d2d5a8f1d0b6f7e4c20c622e984d4adb248b58af
Vladimir <vovar3573@mail.ru>
2023-03-18 12:34:25 +0300

    'Task16_completed'

--- HW3__16_03_2023/task16.py
+++ HW3__16_03_2023/task16.py
@@ -2,13 +2,19 @@
 # Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
 # В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
 
-quantity = int(input('Введите общее число монеток (большее 1): '))
-reshk = -1
-while not(0 < reshk < quantity):
-    reshk = int(input(f'Введите количество тех монеток, что лежат решкой вверх 1 - {quantity-1}: '))
-if 2 * reshk < quantity:
-    print(reshk)
-else:
-    print(quantity-reshk)
+from random import randint
+
+quant = int(input('Введите количество элементов в массиве: '))
+arr = [randint(1, 10) for _ in range(quant)]
+print(arr)
+number = int(input('Введите чиcло X: '))
+dict = {}         # можно было и банальной пробежкой. Но открылась жажда поэкспериментировать со словарем.
+for i in arr:
+    dict[i] = dict.get(i, 0) + 1
+# print(dict)
+k = dict.get(number, 0)
+tx = 'раз'       # возвращение "орфографической красоты"
+if (k % 10 == 2 or k % 10 == 3 or k % 10 == 4) and (k // 10) % 10 != 1: tx = 'раза'
+print(f'Число {number} встречается {k} {tx}. ')
 
 
