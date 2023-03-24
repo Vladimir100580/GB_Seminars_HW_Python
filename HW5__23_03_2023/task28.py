86f120b0385b57c15502af5eebc4d1d01cc7337d
Vladimir <vovar3573@mail.ru>
2023-03-24 19:40:00 +0300

    'task28_completed'

--- HW5__23_03_2023/task28.py
+++ HW5__23_03_2023/task28.py
@@ -1,15 +1,15 @@
 # Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел.
 # Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
 
-from random import randint
 
-n_quantity = int(input('Введите число элементов первого набора: '))
-m_quantity = int(input('Введите число элементов второго набора: '))
-list_n = [randint(0, 30) for _ in range(n_quantity)]
-list_m = [randint(0, 30) for _ in range(m_quantity)]
-print(list_n)
-print(list_m)
-list_n.sort()
-list_m.sort()
+a = int(input('Введите первое слагаемое: '))
+b = int(input('Введите второе слагаемое: '))
 
-print(set(list_m) | set(list_n))
+
+def Sum1(a, b):
+    if b == 0:
+        return a
+    return 1 + Sum1(a, b - 1)  # Решив задачу 26, эта решилась на раз-два.
+
+
+print(f'{a}+{b}={Sum1(a, b)}')
